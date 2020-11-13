532b5a3322bfecd04d52add17849326497b2ded2
Joo Heredia <djjoo007uw@gmail.com>
2020-11-12 23:10:35 -0800

    add questions js

--- app.js
+++ app.js
@@ -12,6 +12,41 @@ let time = questions.length * 15;
 const timer;
 
 function startGame() {
-    home.classList.add('hide');
+    home.classList.add('hide');1
     game.classList.remove('hide');
 }
+
+timeContainer.textContent = time;
+
+timer = setInterval(countDown, 1000);
+
+let questions = [
+    {
+      question: "Inside which HTML element do we put the JavaScript?",
+      choices: ["<script>", "<javascript>", "<js>", "<scripting>"],
+      answer: "<script>"
+    },
+    {
+      question:
+        "What is the correct syntax for referring to an external script called 'xxx.js'?",
+      choices: ["<script href='xxx.js'>", "<script name='xxx.js'>", "<script src='xxx.js'>", "<script file='xxx.js'>"],
+      answer: "<script src='xxx.js'>"
+    },
+    {
+      question: "What are variables used for in JavaScript Programs?",
+      choices: ["Storing numbers, dates, or other values", "Varying randomly", "Validating a form", "None of the above"],
+      answer: "Storing numbers, dates, or other values"
+    },
+    {
+      question:
+        "Which tool can you use to ensure code quality?",
+      choices: ["jQuery", "ESLint", "RequireJS", "Angular"],
+      answer: "ESLint"
+    },
+    {
+      question: 
+        "How do you write 'Hello World' in an alert box?",
+      choices: ["msgBox('Hello World');", "alertBox('Hello World');", "msg('Hello World');" "alert('Hello World');"],
+      answer: "alert('Hello World');"
+    }
+   ];
