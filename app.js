f35c62dbb33a2883313ef2fba0315289df782270
Joo Heredia <djjoo007uw@gmail.com>
2020-11-12 23:43:31 -0800

    add question generate js

--- app.js
+++ app.js
@@ -11,21 +11,42 @@ let currentQuestionIndex = 0;
 let time = questions.length * 15;
 const timer;
 
+// Start Quiz Function
 function startGame() {
     home.classList.add('hide');
-    1
     game.classList.remove('hide');
 }
 
+// Timer Function
 timeContainer.textContent = time;
 
 timer = setInterval(countDown, 1000);
 
+function countDown() {
+    time--;
+    timeContainer.textContent = time;
+    if (time <=0){
+        endGame();
+    }
+}
+
+// Generate Questions
 function generate() {
     let current = questions[currentQuestionIndex];
     questionContainer.textContent = current.question;
 
     buttons.textContent = '';
+
+    current.choices.forEach(
+        function(choice, i) {
+            const optionNode = document.createElement('button');
+            optionNode.classList.add('choice');
+            optionNode.setAttribute('value', choice);
+            optionNode.textContent = i + 1 + '. ' + choice;
+            optionNode.onClick = answerCheck;
+            buttoms.appendChild(optionNode);
+        }
+    );
 }
 
 
