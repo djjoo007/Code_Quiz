8b372a12878d0d445799dcea201a25bb2e6aa44d
Joo Heredia <djjoo007uw@gmail.com>
2020-11-12 23:35:13 -0800

    add question generate  js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const question = document.getElementById('question');
 const submit = document.getElementById('submit');
 const start = document.getElementById('time');
-const buttons = document.getElementById('btns')
+const buttons = document.getElementById('btns');
 const time = document.getElementById('time');
 const result = document.getElementById('result');
 
@@ -21,55 +21,13 @@ timeContainer.textContent = time;
 
 timer = setInterval(countDown, 1000);
 
-let questions = [{
-        question: "Inside which HTML element do we put the JavaScript?",
-        choices: ["<script>", "<javascript>", "<js>", "<scripting>"],
-        answer: "<script>"
-    },
-    {
-        question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
-        choices: ["<script href='xxx.js'>", "<script name='xxx.js'>", "<script src='xxx.js'>", "<script file='xxx.js'>"],
-        answer: "<script src='xxx.js'>"
-    },
-    {
-        question: "What are variables used for in JavaScript Programs?",
-        choices: ["Storing numbers, dates, or other values", "Varying randomly", "Validating a form", "None of the above"],
-        answer: "Storing numbers, dates, or other values"
-    },
-    {
-        question: "Which tool can you use to ensure code quality?",
-        choices: ["jQuery", "ESLint", "RequireJS", "Angular"],
-        answer: "ESLint"
-    },
-    {
-        question: "How do you write 'Hello World' in an alert box?",
-        choices: ["msgBox('Hello World');", "alertBox('Hello World');", "msg('Hello World');", "alert('Hello World');"],
-        answer: "alert('Hello World');"
-    },
-    {
-        question: "Which built-in method calls a function for each element in the array?",
-        choices: ["while()", "loop()", "forEach()", "None of the above"],
-        answer: "forEach()"
-    },
-    {
-        question: "Which of the following function of Array object sorts the elements of an array?",
-        choices: ["toSource()", "sort()", "toString()", "unshift()"],
-        answer: "sort()"
-    },
-    {
-        question: "Which of the following function of String object returns the calling string value converted to lower case while respecting the current locale?",
-        choices: ["toLocaleLowerCase()", "toLowerCase()", "toString()", "substring()"],
-        answer: "toLocaleLowerCase()"
-    },
-    {
-        question: "Which of the following function of Number object returns a string value version of the current number?",
-        choices: ["toString()", "toFixed()", "toLocaleString()", "toPrecision()"],
-        answer: "toString()"
-    },
-    {
-        question: "Which of the following type of variable is visible everywhere in your JavaScript code?",
-        choices: ["local variable", "global variable", "Both of the above", "None of the above"],
-        answer: "global variable"
-    },
+function generate() {
+    let current = questions[currentQuestionIndex];
+    questionContainer.textContent = current.question;
 
-];
+    buttons.textContent = '';
+}
+
+
+
+generate();
